acb1712419403ec5395cf54682ae3265f6b1516c
Tkhorik Dmitry <marvelmd@gmail.com>
2025-03-10 10:29:56 +0300

    Updated keymaps and Obsidian plugin configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- -- telescope
+vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>")
+vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>")
+
+-- tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>")
+
+-- icon picker
+vim.keymap.set("n", "<leader>ic", ":IconPickerNormal<cr>", { noremap = true, silent = true })
+
+-- markdown preview
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
+
+vim.keymap.set("n", "<leader>gd", ":lua require('goto-preview').goto_preview_definition()<CR>")
+vim.keymap.set("n", "<leader>gt", ":lua require('goto-preview').goto_preview_type_definition()<CR>")
+vim.keymap.set("n", "<leader>gi", ":lua require('goto-preview').goto_preview_implementation()<CR>")
+vim.keymap.set("n", "<leader>gp", ":lua require('goto-preview').close_all_win()<CR>")
--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,145 +1,146 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = { "markdown", "snacks_dashboard" },
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = { "markdown", "snacks_dashboard" },
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		"hrsh7th/nvim-cmp",
-	},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+  },
 
-	opts = {
-		completion = {
-			dir = "~/Documents/ObsidianVault",
-			nvim_cmp = true,
-			min_chars = 2,
-		},
+  opts = {
+    completion = {
+      dir = "~/Documents/ObsidianVault",
+      nvim_cmp = true,
+      min_chars = 2,
+    },
 
-		workspaces = {
-			{
-				name = "primary_vault",
-				path = vim.fn.expand("~/Documents/ObsidianVault"),
-				overrides = {
-					notes_subdir = "notes",
-					daily_notes = {
-						folder = "dailies",
-						date_format = "%Y-%m-%d",
-					},
-				},
-			},
-		},
+    workspaces = {
+      {
+        name = "primary_vault",
+        path = vim.fn.expand("~/Documents/ObsidianVault"),
+        overrides = {
+          notes_subdir = "inbox",
+          new_notes_location = "notes_subdir",
+          daily_notes = {
+            folder = "dailies",
+            date_format = "%Y-%m-%d",
+          },
+        },
+      },
+    },
 
-		log_level = vim.log.levels.INFO,
+    log_level = vim.log.levels.INFO,
 
-		daily_notes = {
-			folder = "notes/dailies",
-			date_format = "%Y-%m-%d",
-			alias_format = "%B %-d, %Y",
-			default_tags = { "daily-notes" },
-			template = nil,
-		},
+    daily_notes = {
+      folder = "notes/dailies",
+      date_format = "%Y-%m-%d",
+      alias_format = "%B %-d, %Y",
+      default_tags = { "daily-notes" },
+      template = nil,
+    },
 
-		mappings = {
-			["gf"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			["ch"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true },
-			},
-			["<leader>o"] = {
-				action = function()
-					return require("obsidian").util.smart_action()
-				end,
-				opts = { buffer = true, expr = true },
-			},
-		},
+    mappings = {
+      ["gf"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["ch"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      ["<leader>o"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
 
-		new_notes_location = "notes_subdir",
+    new_notes_location = "notes_subdir",
 
-		note_id_func = function(title)
-			if title then
-				return title:gsub("[^%w%-_ ]", ""):gsub("%s+", "-"):lower()
-			end
-			return tostring(os.time())
-		end,
+    note_id_func = function(title)
+      if title then
+        return title:gsub("[^%w%-_ ]", ""):gsub("%s+", "-"):lower()
+      end
+      return tostring(os.time())
+    end,
 
-		note_path_func = function(spec)
-			local path = spec.dir / tostring(spec.id)
-			return path:with_suffix(".md")
-		end,
+    note_path_func = function(spec)
+      local path = spec.dir / tostring(spec.id)
+      return path:with_suffix(".md")
+    end,
 
-		wiki_link_func = function(opts)
-			return require("obsidian.util").wiki_link_id_prefix(opts)
-		end,
+    wiki_link_func = function(opts)
+      return require("obsidian.util").wiki_link_id_prefix(opts)
+    end,
 
-		markdown_link_func = function(opts)
-			return require("obsidian.util").markdown_link(opts)
-		end,
+    markdown_link_func = function(opts)
+      return require("obsidian.util").markdown_link(opts)
+    end,
 
-		preferred_link_style = "wiki",
+    preferred_link_style = "wiki",
 
-		disable_frontmatter = false,
+    disable_frontmatter = false,
 
-		note_frontmatter_func = function(note)
-			if note.title then
-				note:add_alias(note.title)
-			end
-			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-				for k, v in pairs(note.metadata) do
-					out[k] = v
-				end
-			end
-			return out
-		end,
+    note_frontmatter_func = function(note)
+      if note.title then
+        note:add_alias(note.title)
+      end
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
 
-		templates = {
-			folder = "templates",
-			date_format = "%Y-%m-%d",
-			time_format = "%H:%M",
-			substitutions = {},
-		},
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      substitutions = {},
+    },
 
-		follow_url_func = function(url)
-			vim.fn.jobstart({ "open", url })
-		end,
+    follow_url_func = function(url)
+      vim.fn.jobstart({ "open", url })
+    end,
 
-		follow_img_func = function(img)
-			vim.fn.jobstart({ "qlmanage", "-p", img })
-		end,
+    follow_img_func = function(img)
+      vim.fn.jobstart({ "qlmanage", "-p", img })
+    end,
 
-		picker = {
-			name = "telescope.nvim",
-			note_mappings = { new = "", insert_link = "" },
-			tag_mappings = { tag_note = "", insert_tag = "" },
-		},
+    picker = {
+      name = "telescope.nvim",
+      note_mappings = { new = "", insert_link = "" },
+      tag_mappings = { tag_note = "", insert_tag = "" },
+    },
 
-		sort_by = "modified",
-		sort_reversed = true,
+    sort_by = "modified",
+    sort_reversed = true,
 
-		search_max_lines = 1000,
+    search_max_lines = 1000,
 
-		open_notes_in = "current",
+    open_notes_in = "current",
 
-		ui = { enable = true },
+    ui = { enable = true },
 
-		attachments = {
-			img_folder = "assets/imgs",
-			img_name_func = function()
-				return string.format("%s-", os.time())
-			end,
-			img_text_func = function(client, path)
-				path = client:vault_relative_path(path) or path
-				return string.format("![%s](%s)", path.name, path)
-			end,
-		},
-	},
+    attachments = {
+      img_folder = "assets/imgs",
+      img_name_func = function()
+        return string.format("%s-", os.time())
+      end,
+      img_text_func = function(client, path)
+        path = client:vault_relative_path(path) or path
+        return string.format("![%s](%s)", path.name, path)
+      end,
+    },
+  },
 }
